4d95c7fb48b3795b9f350a92b51b514a5d427f2e
BeanInJ <1272219624@qq.com>
2021-02-01 20:28:04 +0800

    map总结完

--- java_api/java/Test_0_int.java
+++ java_api/java/Test_0_int.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
 
 public class Test_0_int {
     public static void main(String[] args) {
--- java_api/java/Test_17_Map.java
+++ java_api/java/Test_17_Map.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -29,9 +30,49 @@ class MyMap {
             System.out.println(m1.get(mm));
         }
 
-        // Entry 键值对对象，一个entry就是一个键和值
-        // entrySet
-
+        // 遍历：通过Entry对象遍历  （Entry：键值对对象，一个entry就是一个键和值）
+        // Entry 是Map的内部类
+        // Set<Map.Entry<Integer, String>> sme = m1.entrySet();
+        for(Map.Entry<Integer,String> e : m1.entrySet()){
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+        }
 
     }
 }
+
+class MyHashMap{
+    public static void main(String[] args) {
+        // HashMap，，K和V可以为null
+        // 常用方法,详细用法见”Map“里面的
+        HashMap<String,String> m = new HashMap<>();
+        String v1 = m.put("医生", "张三");
+
+        // jdk 9对集合进行了优化
+        // List、Set、Map接口里面添加了一个静态方法of，可以给集合一次性添加多个元素
+        // 使用前提：集合中元素个数已经确定，不再改变
+        // 注意：1、of只适用于List、Set、Map接口，不适用于其实现类
+        //      2、of的返回值是一个不可改变的集合，不能再使用add、put等方法进行添加
+        //      3、在使用of时，不能有重复元素。
+
+            // List<String> l1 = List.of("a","b","c");
+            // Set<String> s1 = Set.of("a","b","c");
+            // Map<Integer,String> m1 = Map.of(1,"a",2,"b",3,"c");
+    }
+}
+
+class MyLinkedHashMap{
+    // LinkedHashMap 有顺序的hashmap，它继承自HashMap
+    // 使用方式和HashMap一样
+}
+
+class MyHashTable{
+    // 特点：同步的、速度慢、K和V不可以为null
+    // 在jdk 1.2 之后被HashMap取代了
+    // 但是他的子类Properties依然在使用
+}
+
+class MyProperties{
+    // Properties是唯一个和IO流结合的集合
+}
+
